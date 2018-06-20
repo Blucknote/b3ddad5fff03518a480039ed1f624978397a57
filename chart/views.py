a204933408d7bb10c86d785bfea616339bbd8315
Blucknote <ccatb@mail.ru>
2018-06-20 19:17:09 +0300

    view with template added

--- chart/views.py
+++ chart/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import chart_row
 
+def chart_rows(request):
+    crows = chart_row.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'chart/chart_rows.html', {'crows':crows})
 # Create your views here.
