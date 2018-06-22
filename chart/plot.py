d015a701f3dd22091a5df604ee49ba44023ff58b
Blucknote <ccatb@mail.ru>
2018-06-22 23:24:01 +0300

    celery integration (test)

--- chart/plot.py
+++ chart/plot.py
@@ -4,10 +4,18 @@ matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from time import time
+from celery import Celery
+from .models import chart_row
 
-def chart_make(func, interval, dt):
-    _interval = timedelta(days = interval)
-    _dt       = timedelta(hours = dt)
+app = Celery()
+#broker='amqp://', result_backend = 'redis://'
+
+@app.task(max_retries=1)
+def chart_make(dbid):
+    obj = chart_row.objects.get(pk= dbid)
+    print(obj.func)
+    interval = timedelta(days = obj.period)
+    dt       = timedelta(hours = obj.dt)
 
     t = np.arange(datetime.now() - _interval, datetime.now(), _dt)
 
@@ -16,3 +24,10 @@ def chart_make(func, interval, dt):
     fig_name = './chs/%s.png' % time()
     plt.savefig(fig_name)
     return fig_name[1:] 
+
+def main():
+    x = chart_make.delay(3)
+    print(x.get())
+
+if __name__ == '__main__':
+    main()
--- chart/views.py
+++ chart/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.shortcuts import redirect
 from .models import chart_row
 from .forms import chart_row_add
-from . import plot
+from .plot import chart_make
 
 def chart_rows(request):
     crows = chart_row.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -15,8 +15,9 @@ def crow_add(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.published_date = timezone.now()
-            post.chart = plot.chart_make(post, int(post.period), int(post.dt))
             post.save()
+            chart_make.apply_async((post.id, ), countdown = 3, expires= 15)
+            
             return redirect('/')
     else:
         form = chart_row_add()
